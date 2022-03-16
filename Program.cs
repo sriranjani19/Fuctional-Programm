eceb8136e03cde747c1f23ee310fc5c83b4f5a6d
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-03-16 15:48:59 +0530

    add.programm

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace BasicCoreProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Functional Programing");
+
+            //flipCoin.flipTheCoin();
+
+            //LeapYear.leapyear();
+
+            //HarmonicNumber.HarNum();
+
+            //PrimeFactor.primefactor();
+
+            //QuotientRemainder.FindQuotientAndRemainder();
+
+            //NumberSwap.SwapTwoNUmbers();
+
+            //OddEven.OddorEven();
+
+            //VowelAndConstant.CheckVowelAndConstant();
+
+            //PrimeNumber.CheckPrimeNumber();
+
+            LargestNumber.FindLargestNumber();
+
+        }
+    }
+}
